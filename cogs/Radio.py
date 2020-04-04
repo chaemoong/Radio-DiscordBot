9c8174aec38931a50b16c0c156098e2accc3ccae
chaemoong <49019734+chaemoong@users.noreply.github.com>
2020-04-05 03:07:47 +0900

    라디오 시작, 종료 부분 업데이트

--- cogs/Radio.py
+++ cogs/Radio.py
@@ -13,15 +13,15 @@ class Radio(Cog):
         self.file2 = 'data/radio/settings.json'
         self.data2 = dataIO.load_json(self.file2)
 
-    @group(name='settings', pass_context=True, aliases=['ㄴㄷㅅ샤ㅜㅎㄴ', 'tjfwjd', '설정'])
+    @group(name='settings', no_pm=True, pass_context=True, aliases=['ㄴㄷㅅ샤ㅜㅎㄴ', 'tjfwjd', '설정'])
     async def settings(self, ctx):
         if ctx.invoked_subcommand is None:
             em = Embed(colour=0xffff00)
-            em.add_field(name='라디오 관리자 설정', value='DJ - 라디오 DJ역할을 설정합니다!\n알람 - 라디오 시작할때 시작을 알릴 멘션을 선택합니다!\n채널 - 라디오 보이스 채널을 설정합니다!')
+            em.add_field(name='라디오 관리자 설정', value='DJ - 라디오 DJ역할을 설정합니다!\n알람 - 라디오 시작할때 시작을 알릴 멘션을 선택합니다!\n채널 - 라디오 시작혹은 종료할때 시작 혹은 종료를 알릴 채널을 선택합니다!')
             return await ctx.send(embed=em)
 
 
-    @group(name='radio', pass_context=True, aliases=['ㄱㅁ야ㅐ', 'fkeldh', '라디오'])
+    @group(name='radio', no_pm=True, pass_context=True, aliases=['ㄱㅁ야ㅐ', 'fkeldh', '라디오'])
     async def radio(self, ctx):
         if ctx.invoked_subcommand is None:
             em = Embed(colour=0xffff00)
@@ -33,10 +33,13 @@ class Radio(Cog):
         author = ctx.author
         server = ctx.guild
         await ctx.message.delete()
+        serverdata = self.data2.get(str(server.id))
         if author.voice == None:
             return await ctx.send('> 라디오 시작전 보이스 채널에 입장해주세요!')
-        if not self.data.get(str(server.id)) == None:
+        if not self.data.get(str(server.id)) == None or not self.data.get(str(server.id)) == {}:
             return await ctx.send('> 현재 진행중인 라디오가 있습니다! 현재는 예약만 가능합니다!')
+        if serverdata == None:
+            return await ctx.send(f'> 라디오 관련 데이터가 설정되있지 않습니다! `{ctx.prefix}settings`로 설정해주세요!')
         if message == None:
             message = f'{author.name} 님의 라디오가 시작합니다!'
         a = await ctx.send('라디오를 시작하시겠습니까?')
@@ -52,24 +55,35 @@ class Radio(Cog):
         except TimeoutError:
             return await a.edit(content='> 라디오 시작을 취소하였습니다!')
         if str(reaction.emoji) == '⭕':
+            self.data[str(ctx.guild.id)] = {}
+            self.data[str(ctx.guild.id)].update({"dj": author.id})
+            dataIO.save_json(self.file, self.data)
+            em = Embed(colour=0xffff00)
+            em.add_field(name='라디오 시작 알림', value=message)
+            await self.bot.get_channel(serverdata.get('channel')).send(content=get(server.roles, id=serverdata.get('role')),embed=em)
             return await a.edit(content='> 라디오를 시작하겠습니다!')
         if str(reaction.emoji) == '❌':
             return await a.edit(content='> 라디오 시작을 취소하였습니다!')
-        self.data[str(ctx.guild.id)] = {}
-        self.data[str(ctx.guild.id)].update({"dj": author.id})
-        dataIO.save_json(self.file, self.data)
 
     @radio.command(name='종료', no_pm=True)
-    async def 종료(self, ctx, *, message=None):
+    async def 종료(self, ctx):
         author = ctx.author
         server = ctx.guild
         await ctx.message.delete()
+        serverdata = self.data2.get(str(server.id))
         if author.voice == None:
-            return await ctx.send('> 라디오 시작전 보이스 채널에 입장해주세요!')
-        if not self.data.get(str(server.id)) == None:
-            return await ctx.send('> 현재 진행중인 라디오가 있습니다! 현재는 예약만 가능합니다!')
-        if message == None:
-            message = f'{author.name} 님의 라디오가 시작합니다!'
+            return await ctx.send('> 라디오 종료전 보이스 채널에 입장해주세요!')
+        if serverdata == None:
+            return await ctx.send(f'> 라디오 관련 데이터가 설정되있지 않습니다! `{ctx.prefix}settings`로 설정해주세요!')
+        if self.data.get(str(server.id)) == None:
+            return await ctx.send('> 현재 진행중인 라디오가 없습니다!')
+        message = f'{author.name}님의 라디오가 종료되었습니다!\nDJ분들 수고하셨습니다!'
+        self.data[str(ctx.guild.id)] = {}
+        dataIO.save_json(self.file, self.data)
+        em = Embed(colour=0xffff00)
+        em.add_field(name='라디오 종료 알림', value=message)
+        await self.bot.get_channel(serverdata.get('channel')).send(content=get(server.roles, id=serverdata.get('role')),embed=em)
+        return await a.edit(content='> 라디오를 종료하겠습니다!')
 
 def check_folder():
     if not os.path.exists('data/radio'):
